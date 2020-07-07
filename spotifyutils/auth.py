4fa0fbb3f579c12fc76dd3d38e20676957ddacd1
evanH13 <evanheringer@gmail.com>
2020-07-06 21:05:52 -0700

    Fix if/else congif block, configfile optional

--- spotifyutils/auth.py
+++ spotifyutils/auth.py
@@ -18,14 +18,14 @@ def user_auth(redirect_uri: str, client_id: str):
     REDIRECT_URI = redirect_uri
     base_url = "https://accounts.spotify.com/authorize"
     response_type = 'code'
-    scope = 'user-read-email'
+    scope = ['user-read-email']
     show_dialog = 'true'
 
     PARAMS = {
         'client_id': client_id,
         'response_type': response_type,
         'redirect_uri': redirect_uri,
-        'scope': scope,
+        'scope': (''.join(scope)),
         'show_dialog': show_dialog
     }
 
--- spotifyutils/config.py
+++ spotifyutils/config.py
@@ -6,12 +6,13 @@ from spotifyutils.auth import user_auth, secure_server, server, get_tokens
 def configuration(**kwargs: dict):
     """ If configfile arg is passed, check if any other args are passed. If additional args are passed, overwrite any existing args 
     specified in configfile. If additional arguments are NOT passed, simply read the values stored in the configfile. 
-    If configfile is not passed, use the args passed in the CLI OR take user input. """
+    If configfile is not passed, use the args passed in the CLI OR take user input. If the user chooses to entre a location for the configfile,
+    a config file will be created. """
 
+    config = configparser.ConfigParser()
 
     if kwargs['configfile']:
         configfile = kwargs['configfile']
-        config = configparser.ConfigParser()
         if kwargs['client_id'] or kwargs['client_secret'] or kwargs['redirect_uri']:
             config['spotifyutils'] = {}
 
@@ -29,17 +30,16 @@ def configuration(**kwargs: dict):
             client_id = config.get('spotifyutils', 'client_id')
             client_secret = config.get('spotifyutils', 'client_secret')
             redirect_uri = config.get('spotifyutils', 'redirect_uri')
+            
     
-
     else:
         client_id = kwargs['client_id'] or input('Client ID: ')
         client_secret = kwargs['client_secret'] or getpass.getpass(prompt='Client Secret: ')
         redirect_uri = kwargs['redirect_uri'] or input('Redirect URI: ')
-        configfile = input('User Config File Location: ')
+        configfile = input('(optional) User Config File Location: ')
         
         if configfile:
             configfile = os.path.join(os.path.expanduser('~'), configfile)
-            config = configparser.ConfigParser()
             config['spotifyutils'] = {}
             config_params = {
                 'client_id': client_id,
